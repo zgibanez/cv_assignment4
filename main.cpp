372ee777bd003cc63b5be6be2fcd859a06832d01
zgibanez <zoiletum@gmail.com>
2017-04-04 15:05:41 +0200

    añadidos comentarios de lo que queda por hacer en main

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@ using namespace cv;
 
 void main()
 {
+	//TODO
+	//1) El primer tamaño probado debe ser el de la ventana
+	//2) Probar threshold negativo para asegurar que se tengan ovejas
+	//3) Petar al trainer con negativas
+	//4) Investigar las dimensiones de una oveja
+	//5) En seleccion de ROI, investigar el porcentaje de imagen ocupado por la oveja(máximo, mínimo y promedio)
+	//	6) Preparar Area under the curve
+	//	7) Hacer funcion para area of intersection (IoU)
+
 	Trainer trainer = Trainer();
 
 	//trainer.buildROISet("dataset\\positive_training\\");
@@ -23,7 +32,8 @@ void main()
 	tester.setSVM(trainer.getTrainedSVM());
 	//tester.test("dataset\\testing\\");
 	
-	Mat img = imread("4.jpg", IMREAD_GRAYSCALE);
+	Mat img = imread("2.jpg", IMREAD_GRAYSCALE);
+	//tester.test("dataset\\testing\\");
 	//cout << a.rows << endl;
 	vector<Match> results = tester.getPositiveMatches(img,1);
 	//tester.drawPositiveMatchBB(results, img.clone());
